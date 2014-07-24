0659c53084839e0f8666a8b65c1f75fc30521441
Brian Allen <brian@SML132TO-Brian-Allen.local>
2014-07-24 16:38:20 -0400

    committing updates

--- dilemma/VotePicturesViewController.m
+++ dilemma/VotePicturesViewController.m
@@ -48,17 +48,15 @@
     int r2querylimit = 25;
     NSInteger *query2limitnum = &r2querylimit;
     PhotoSetQuery.limit = *query2limitnum;
+   
+    [PhotoSetQuery includeKey:@"Photos"];
+    
     
     [PhotoSetQuery findObjects];
     
-    
-    
-    
-     PFQuery *photosQuery = [PFQuery queryWithClassName:@"Photo"];
-    [photosQuery whereKey:@"objectId"
  
    
-    [photosQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+    [PhotoSetQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
         if  (!error)
         {
             PhotoSetObjects =  objects;
@@ -67,6 +65,8 @@
             int j = 0;
             NSArray *photoList = [[NSArray alloc] init];
             photoList = [firstPhotoSet objectForKey:@"Photos"];
+            NSArray *voteList = [[NSArray alloc] init];
+            voteList = [firstPhotoSet objectForKey:@"PhotoVotes"];
             
             
             for (PFObject *photoObj in photoList)
@@ -75,6 +75,7 @@
                 
                 
                 PFFile *imgFile = [photoObj objectForKey:@"imageFile"];
+                
                 //get URL from imgfile
                 NSString *imgurl;
                 
@@ -85,19 +86,47 @@
                 
                 if(j==1)
                 {
-                    [self.imgView1 setImageWithURL:[NSURL URLWithString:imgurl] usingActivityIndicatorStyle:(UIActivityIndicatorViewStyle)activityStyle ];
+                      [self.imgView1 setImageWithURL:[NSURL URLWithString:imgurl] usingActivityIndicatorStyle:(UIActivityIndicatorViewStyle)activityStyle ];
+                    UILabel *voteScore = [[UILabel alloc] init];
+                    NSNumber *votenum = [voteList objectAtIndex:0];
+                    voteScore.text = [votenum stringValue];
+                    voteScore.textColor = [UIColor whiteColor];
+                    voteScore.frame = self.imgView1.bounds;
+                    
+                    [self.imgView1 addSubview:voteScore];
+                 
                 }
                 if(j==2)
                 {
                     [self.imgView2 setImageWithURL:[NSURL URLWithString:imgurl] usingActivityIndicatorStyle:(UIActivityIndicatorViewStyle)activityStyle ];
+                    UILabel *voteScore = [[UILabel alloc] init];
+                    NSNumber *votenum = [voteList objectAtIndex:1];
+                    voteScore.text = [votenum stringValue];
+                    voteScore.textColor = [UIColor whiteColor];
+                    voteScore.frame = self.imgView1.bounds;
+                    [self.imgView1 addSubview:voteScore];
+                    
+                    
                 }
                 if(j==3)
                 {
                     [self.imgView3 setImageWithURL:[NSURL URLWithString:imgurl] usingActivityIndicatorStyle:(UIActivityIndicatorViewStyle)activityStyle ];
+                    UILabel *voteScore = [[UILabel alloc] init];
+                    NSNumber *votenum = [voteList objectAtIndex:2];
+                    voteScore.text = [votenum stringValue];
+                    voteScore.textColor = [UIColor whiteColor];
+                    voteScore.frame = self.imgView1.bounds;
+                    [self.imgView1 addSubview:voteScore];
                 }
                 if(j==4)
                 {
                     [self.imgView4 setImageWithURL:[NSURL URLWithString:imgurl] usingActivityIndicatorStyle:(UIActivityIndicatorViewStyle)activityStyle ];
+                    UILabel *voteScore = [[UILabel alloc] init];
+                    NSNumber *votenum = [voteList objectAtIndex:3];
+                    voteScore.text = [votenum stringValue];
+                    voteScore.textColor = [UIColor whiteColor];
+                    voteScore.frame = self.imgView1.bounds;
+                    [self.imgView1 addSubview:voteScore];
                 }
                 
             }
